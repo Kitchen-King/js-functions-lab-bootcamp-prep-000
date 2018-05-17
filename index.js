a3a87bb2fc56605982bec32d27bdd470c29ca9f8
Kyle Spencer <spencerk2@vcu.edu>
2018-05-17 21:04:35 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,4 @@
+function happyHolidays() {return `Happy Holidays!`}
+function happyHolidaysTo(individual) {return `happyHolidays, ${individual}`}
+function happyHolidayTo(holiday, individual) {return `Happy ${holiday}, ${individual}!`}
+function holidayCountdown(holiday, daysUntil) {return `It's ${daysUntil} days until ${holiday}!`}
